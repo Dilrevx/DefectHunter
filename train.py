223b5cb3d8399862fdc2ee504a0ff107f3ecf61f
Dilrevx <1747693932@qq.com>
2025-03-19 17:19:56 +0800

    fix: specify `numpy<2`, `keras==2.7`

--- train.py
+++ train.py
@@ -1,9 +1,8 @@
 import keras_metrics as km
 import numpy as np
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.models import Model
-from tensorflow.keras.layers import Dense, Flatten, Input, concatenate, \
-    Dropout, Reshape
+from tensorflow.keras.layers import Dense, Flatten, Input, concatenate, Dropout, Reshape
 from tensorflow.keras.optimizers import Adam
 
 from network.attention import SinusoidalPositionEmbedding
@@ -41,10 +40,18 @@ sin_cfg = SinusoidalPositionEmbedding(output_dim=200, merge_mode="mul")(input_cf
 sin_ast = SinusoidalPositionEmbedding(output_dim=200, merge_mode="mul")(input_ast)
 shape_emb1 = Reshape((1, 768))(sin_emb)
 
-conforencode_emb = ConformerEncoder(embedding_dim=768, num_heads=8, feed_forward_dim=1024, num_blocks=12)(shape_emb1)
-conforencode_ast = ConformerEncoder(embedding_dim=200, num_heads=8, feed_forward_dim=1024, num_blocks=12)(sin_ast)
-conforencode_cfg = ConformerEncoder(embedding_dim=200, num_heads=8, feed_forward_dim=1024, num_blocks=12)(sin_cfg)
-conforencode_dfg = ConformerEncoder(embedding_dim=200, num_heads=8, feed_forward_dim=1024, num_blocks=12)(sin_dfg)
+conforencode_emb = ConformerEncoder(
+    embedding_dim=768, num_heads=8, feed_forward_dim=1024, num_blocks=12
+)(shape_emb1)
+conforencode_ast = ConformerEncoder(
+    embedding_dim=200, num_heads=8, feed_forward_dim=1024, num_blocks=12
+)(sin_ast)
+conforencode_cfg = ConformerEncoder(
+    embedding_dim=200, num_heads=8, feed_forward_dim=1024, num_blocks=12
+)(sin_cfg)
+conforencode_dfg = ConformerEncoder(
+    embedding_dim=200, num_heads=8, feed_forward_dim=1024, num_blocks=12
+)(sin_dfg)
 
 shape_emb2 = Reshape((1, 768))(conforencode_emb)
 
@@ -60,18 +67,37 @@ drop_1 = Dropout(0.1)(dense_1)
 z = Dense(2, activation="softmax")(drop_1)
 
 model = Model(inputs=[input_emb, input_dfg, input_cfg, input_ast], outputs=z)
-model.compile(optimizer=Adam(learning_rate=1e-5), loss=["sparse_categorical_crossentropy"],  # 8e-7
-              metrics=["accuracy", km.sparse_categorical_f1_score(), km.sparse_categorical_precision(),
-                       km.sparse_categorical_recall()])
+model.compile(
+    optimizer=Adam(learning_rate=1e-5),
+    loss=["sparse_categorical_crossentropy"],  # 8e-7
+    metrics=[
+        "accuracy",
+        km.sparse_categorical_f1_score(),
+        km.sparse_categorical_precision(),
+        km.sparse_categorical_recall(),
+    ],
+)
 model.summary()
-checkpoints = ModelCheckpoint(filepath='model/ck/weights.{epoch:04d}.hdf5', monitor="val_loss", verbose=1,
-                              save_weights_only=True, period=1)
-EarlyStop=EarlyStopping(monitor='val_accuracy', patience=3,verbose=1, mode='max')
+checkpoints = ModelCheckpoint(
+    filepath="model/ck/weights.{epoch:04d}.hdf5",
+    monitor="val_loss",
+    verbose=1,
+    save_weights_only=True,
+    period=1,
+)
+EarlyStop = EarlyStopping(monitor="val_accuracy", patience=3, verbose=1, mode="max")
 
 history = model.fit(
-    [x_train_emb, x_train_dfg, x_train_cfg, x_train_ast], y_train,
+    [x_train_emb, x_train_dfg, x_train_cfg, x_train_ast],
+    y_train,
     validation_data=([x_val_emb, x_val_dfg, x_val_cfg, x_val_ast], y_val),
-    epochs=50, batch_size=64, verbose=2, callbacks=[checkpoints,EarlyStop])
-score = model.evaluate([x_test_emb, x_test_dfg, x_test_cfg, x_test_ast], y_test, verbose=0, batch_size=128)
+    epochs=50,
+    batch_size=64,
+    verbose=2,
+    callbacks=[checkpoints, EarlyStop],
+)
+score = model.evaluate(
+    [x_test_emb, x_test_dfg, x_test_cfg, x_test_ast], y_test, verbose=0, batch_size=128
+)
 print(score)
 print(model.metrics_names)
